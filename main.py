2e51efe32ee2f364b0ce6083b61aac699dc5c0e9
jsm24c <xmartjs@gmail.com>
2025-09-09 13:43:47 -0400

    imports

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import yfinance as yf
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import root_mean_squared_error
