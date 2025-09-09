b6bb4f5a214026fd236da7a362a1e0755ad42a90
jsm24c <xmartjs@gmail.com>
2025-09-09 14:40:45 -0400

    created LSTM class

--- main.py
+++ main.py
@@ -43,3 +43,22 @@ Y_train = torch.from_numpy(data[:train_size, -1, :])    #the last element
 
 X_test = torch.from_numpy(data[train_size:, :-1, :])
 Y_test = torch.from_numpy(data[train_size:, -1, :])
+
+class LSTMModel(nn.Module):
+
+    def __init__(self,input_dim, hidden_dim, num_layers, output_dim):
+        super(LSTMModel, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+
+        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim, device=device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim, device=device)
+
+        out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
+        out = self.fc(out[:, -1, :])
+        return out
+    
